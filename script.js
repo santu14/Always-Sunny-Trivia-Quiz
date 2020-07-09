3c17a04c2f18621fab5186b28032758d04b86c0e
Santiago Solana <santiago.sjs@gmail.com>
2020-07-08 20:19:39 -0400

    Cleaner logic

--- script.js
+++ script.js
@@ -2,41 +2,46 @@ $(document).ready(function () {
 
     var questions = ["poopoo?", "peeppe?", "both?"];
 
-    var answers = {
-        set1: ["yes", "no", "maybe", "leave me alone"],
-        set2: ["maybe", "eww gross", "yeehaw", "yes chef!"],
-        set3: ["omg how u knowww", "how could you", "yeah i deeed datt", "shhhhh"]
-    };
-    //  var answers = [
+    // var answers = {
+    //     set1: ["yes", "no", "maybe", "leave me alone"],
+    //     set2: ["maybe", "eww gross", "yeehaw", "yes chef!"],
+    //     set3: ["omg how u knowww", "how could you", "yeah i deeed datt", "shhhhh"]
+    // };
+     var answers = [
          
-    //      {set: ["yes", "no", "maybe", "leave me alone"]},
-    //      {set: ["maybe", "eww gross", "yeehaw", "yes chef!"]},
-    //      {set: ["omg how u knowww", "how could you", "yeah i deeed datt", "shhhhh"]}
-    //  ];
+         {set: ["yes", "no", "maybe", "leave me alone"]},
+         {set: ["maybe", "eww gross", "yeehaw", "yes chef!"]},
+         {set: ["omg how u knowww", "how could you", "yeah i deeed datt", "shhhhh"]}
+     ];
    
 
     var display = $("#display");
 
-    var correctAnswer = ["yes", "yeehaw",];
+    var correctAnswer = ["yes", "yeehaw","Shhhhh"];
 
     var x = 0;
     var y =0;
     var score = 0;
 
     $(".start-btn").on("click", function () {
-        questionGenerator(questions, answers.set1, correctAnswer);
+        nextQuestion ();
     });
     
-    function questionGenerator(question, answer, correct) {
+    function questionGenerator(question) {
         
         display.empty();
-        display.append("<h1>").text(question[x]);
+        display.append("<h1>").text(question);
         display.append("<br>");
         display.append("<hr>");
 
         x++;
         console.log(x);
 
+        
+        return x;
+    };
+    console.log(answers[0]);
+    function buttonGenerator ( answer, correct){
         for (i = 0; i < answer.length; i++) {
             var btn = $("<button>").addClass("btn answer-button");
             btn.text(answer[i]);
@@ -54,16 +59,21 @@ $(document).ready(function () {
                 $("#response").append("<hr>");
                 $("#response").append("<h3>").text("correct!");
                 score += 10;
+                nextQuestion ()
             } else {
                 $("#response").prepend("<br> <hr> <h3>").text("wrong!");
+                nextQuestion ()
             }
     
         });
-        return x;
-    };
+        y++;
+        return y;
+    }
 
     function nextQuestion (){
-        
+        questionGenerator(questions[x]);
+        buttonGenerator ( answers[y].set, correctAnswer);
+        console.log(score);
     }
 
 });
