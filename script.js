da1b1711ade85c903d5139206458945795c9b125
Santiago Solana <santiago.sjs@gmail.com>
2020-07-08 22:25:49 -0400

    further improvments to logic added game end function and fine tuned timer/scoring system

--- script.js
+++ script.js
@@ -1,47 +1,68 @@
 $(document).ready(function () {
 
     var questions = ["poopoo?", "peeppe?", "both?"];
+    var answers = [
 
-    // var answers = {
-    //     set1: ["yes", "no", "maybe", "leave me alone"],
-    //     set2: ["maybe", "eww gross", "yeehaw", "yes chef!"],
-    //     set3: ["omg how u knowww", "how could you", "yeah i deeed datt", "shhhhh"]
-    // };
-     var answers = [
-         
-         {set: ["yes", "no", "maybe", "leave me alone"]},
-         {set: ["maybe", "eww gross", "yeehaw", "yes chef!"]},
-         {set: ["omg how u knowww", "how could you", "yeah i deeed datt", "shhhhh"]}
-     ];
-   
+        { set: ["yes", "no", "maybe", "leave me alone"] },
+        { set: ["maybe", "eww gross", "yeehaw", "yes chef!"] },
+        { set: ["omg how u knowww", "how could you", "yeah i deeed datt", "shhhhh"] }
+    ];
+    var correctAnswer = ["yes", "yeehaw", "shhhhh"];
+
 
     var display = $("#display");
-
-    var correctAnswer = ["yes", "yeehaw","Shhhhh"];
+    var feedback = $("#feedback");
 
     var x = 0;
-    var y =0;
+    var y = 0;
     var score = 0;
+    var gameEnd;
+
+    var timeLeft = 75;
+    var timer = $("#timer");
+    timer.text(timeLeft);
+    
+    function countdown() {
+
+        var countdownTimer  = setInterval(function(){
+            if (!gameEnd){
+                timeLeft--;
+                timer.text(timeLeft);
+            } else {
+                clearInterval(countdownTimer);
+            };
+        }, 1000);
+        timer.text(timeLeft);
+    };
+
+
 
     $(".start-btn").on("click", function () {
-        nextQuestion ();
+        gameEnd = false;
+        nextQuestion();
+        countdown();
     });
-    
+
+
+
     function questionGenerator(question) {
-        
+
         display.empty();
         display.append("<h1>").text(question);
         display.append("<br>");
         display.append("<hr>");
-
         x++;
         console.log(x);
 
-        
-        return x;
     };
-    console.log(answers[0]);
-    function buttonGenerator ( answer, correct){
+
+
+
+
+    function buttonGenerator(answer, correct) {
+
+        feedback.animate({ opacity: "0.00" },1000);
+
         for (i = 0; i < answer.length; i++) {
             var btn = $("<button>").addClass("btn answer-button");
             btn.text(answer[i]);
@@ -51,29 +72,46 @@ $(document).ready(function () {
         $(".answer-button").on("click", function () {
             console.log(this.innerHTML);
 
-    
+            feedback.css({ opacity: "1" });
+
             if (this.innerHTML === correct[0] ||
                 this.innerHTML === correct[1] ||
                 this.innerHTML === correct[2]) {
-    
-                $("#response").append("<hr>");
-                $("#response").append("<h3>").text("correct!");
+                    
                 score += 10;
-                nextQuestion ()
+                feedback.append("<hr>");
+                feedback.append("<h3>").text("correct!");
+                
+                nextQuestion()
             } else {
-                $("#response").prepend("<br> <hr> <h3>").text("wrong!");
-                nextQuestion ()
+                timeLeft -= 10;
+                timer.text(timeLeft)
+                feedback.prepend("<br> <hr> <h3>").text("wrong!");
+                nextQuestion();
             }
-    
         });
+        
         y++;
-        return y;
     }
 
-    function nextQuestion (){
-        questionGenerator(questions[x]);
-        buttonGenerator ( answers[y].set, correctAnswer);
+    function nextQuestion() {
+        if (x >= questions.length){
+        score += timeLeft;
+        endGame();
+        
+ 
+        }else {
+            questionGenerator(questions[x]);
+            buttonGenerator(answers[y].set, correctAnswer);
+        }
         console.log(score);
+    }
+
+    function endGame (){
+        display.empty();
+        gameEnd = true;
+        display.append("<h1>").text("das it boi you scored " + score + " points");
+
     }
 
 });
